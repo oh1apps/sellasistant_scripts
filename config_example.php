96e930241fea640e10dc3fc2a537a5b1203c12bc
GK <komorovg@outlok.com>
2025-04-12 01:00:21 +0200

    update 1 file and create 1 file

---
+++ config_example.php
@@ -0,0 +1,10 @@
+<?php
+
+//////////////////////////////// ZMIEŃ NAZWĘ TEGO PLIKI NA config.php ////////////////////////////////
+
+$mapper_password = "admin"; // Hasło do panelu mappera produktów
+$shop_id = 0; // ID sklepu Shoper (znajdziesz w adresie URL, przchodząc do Integracje > Sklepy > Edytuj)
+
+$sa_panel_url = "https://example.sellasist.pl"; // Adres panelu Sellasist
+$sa_api_url = "https://example.sellasist.pl/api/v1/"; // Adres API Sellasist
+$sa_api_key = ""; // Klucz API Sellasist
